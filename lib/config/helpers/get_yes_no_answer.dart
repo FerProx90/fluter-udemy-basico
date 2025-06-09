d58b60601576f8986475f441a2b4693dfbc553e5
Antonio Meza <52209606+novamar-programmer2@users.noreply.github.com>
2025-06-09 17:58:22 -0600

    scroll botom al enviar mensajes y respuestas del taker

---
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:hello_wordl/domain/entities/message.dart';
+import 'package:hello_wordl/infrastructure/models/yes_no_model.dart';
+
+class GetYesNoAnswer {
+  final _dio = Dio();
+  
+  Future<Message> getAnswer() async {
+    final response = await _dio.get('https://yesno.wtf/api');
+    final yesNoMode = YesNoModel.fromJsonMap(response.data);
+    
+    return Message(text: yesNoMode.answer, fromWho: FromWho.him, imageUrl: yesNoMode.image);
+  }
+}
--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:hello_wordl/config/helpers/get_yes_no_answer.dart';
 import 'package:hello_wordl/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
+
   List<Message> messageList = [
     Message(text: "I'm the Undertaker", fromWho: FromWho.him),
     Message(text: "es el enterrador?", fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
+    if (text.endsWith('?')){
+      await hisReply();
+    }
+
     notifyListeners(); // para notificar a los que escuchan en el provider y recarguen sus widgets
+    moveScrollToBottom();
+  }
+
+  Future<void> hisReply() async {
+    final hisMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(hisMessage);
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent, // Posicion
+      duration: const Duration(milliseconds: 300), // duracion de animacion
+      curve: Curves.easeOut, // tipo de animacion
+    );
   }
 }
